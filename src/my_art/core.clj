fdbefe2859199c8afa06aed7a9fdee44639650fa
Pieter Breed <pieter.breed@allangray.co.za>
2016-05-20 10:09:20 +0200

    final save game from office

--- src/my_art/core.clj
+++ src/my_art/core.clj
@@ -2,31 +2,7 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
-(defn load-image [path]
-  nil)
 
-(defn make-fitness-function [image-path]
-  (let [target-img (q/load-image image-path)
-        dest-pixels (q/pixels target-img)
-        nr-pixels (count dest-pixels)]
-    [target-img
-     (fn [image]
-       (let [child-pixels (q/pixels image)
-             child-nr-pixels (count child-pixels)]
-         (when (not= nr-pixels child-nr-pixels)
-           (throw (Exception. "The images do not have the same number of pixels")))
-         (as-> (map #(Math/abs (- %1 %2)) dest-pixels child-pixels) $
-               (reduce + $)
-               (Math/sqrt $))))
-     (.-width target-img)
-     (.-height target-img)]))
-
-
-(defn setup []
-  (q/frame-rate 30)
-  (q/smooth)
-  (let [res {:fitness-fn (make-fitness-function "C:\\Users\\Pieterbr\\Pictures\\rd_team.jpg" )}]
-    (println (str "hierso: " res))))
 
 (defn make-perc-of-height-fn [height-perc]
   (fn []
@@ -44,14 +20,17 @@
   (repeatedly #(/ (- 50 (rand-int 100))
                   100)))
 
-(defn adjust-values [vs error_rate]
+(defn may-trigger-change? [error-rate]
+  (>= error-rate
+      (/ (rand-int 100) 100)))
+
+(defn adjust-values [vs error-rate]
   (loop [result []
          avs (get-adjustment-values)
          x (first vs)
          rst (rest vs)]
     (if (nil? x) result
-        (let [v (if (>= error_rate
-                        (/ (rand-int 100) 100))
+        (let [v (if (may-trigger-change? error-rate)
                   x
                   (as-> (first avs) $
                         (+ x $)
@@ -66,7 +45,9 @@
   {:type :ellipse
    :draw-fn q/ellipse
    :make-child (fn [er_perc]
-                 (apply make-ellipse (adjust-values [x y width height] er_perc)))
+                 (if (may-trigger-change? er_perc)
+                   (make-random-shape)
+                   (apply make-ellipse (adjust-values [x y width height] er_perc))))
    :args [(make-perc-of-width-fn x)
           (make-perc-of-height-fn y)
           (make-perc-of-width-fn width)
@@ -75,7 +56,10 @@
 (defn make-arc [x y width height start stop]
   {:type :arc
    :draw-fn q/arc
-   :make-child (fn [ep] (apply make-arc (adjust-values [x y width height start stop] ep)))
+   :make-child (fn [ep]
+                 (if (may-trigger-change? ep)
+                   (make-random-shape)
+                   (apply make-arc (adjust-values [x y width height start stop] ep))))
    :args [(make-perc-of-width-fn x)
           (make-perc-of-height-fn y)
           (make-perc-of-width-fn width)
@@ -86,7 +70,10 @@
 (defn make-triangle [x1 y1 x2 y2 x3 y3]
   {:type :triangle
    :draw-fn q/triangle
-   :make-child (fn [ep] (apply make-triangle (adjust-values [x1 y1 x2 y2 x3 y3] ep)))
+   :make-child (fn [ep]
+                 (if (may-trigger-change? ep)
+                   (make-random-shape)
+                   (apply make-triangle (adjust-values [x1 y1 x2 y2 x3 y3] ep))))
    :args [(make-perc-of-width-fn x1)
           (make-perc-of-height-fn y1)
           (make-perc-of-width-fn x2)
@@ -97,10 +84,14 @@
 (defn make-fill-color [r g b]
   {:type :fill-color
    :draw-fn q/fill
-   :make-child (fn [ep] (apply make-fill-color (adjust-values [r g b] ep)))
+   :make-child (fn [ep]
+                 (if (may-trigger-change? ep)
+                   (make-random-shape)
+                   (apply make-fill-color (adjust-values [r g b] ep))))
    :args [(constantly (* r 255 ))
           (constantly (* g 255))
-          (constantly (* b 255))]})
+          (constantly (* b 255))
+          (constantly 150)]})
 
 (defn make-random-shape []
   (let [stream (repeatedly #(/ (rand-int 100) 100))
@@ -120,9 +111,9 @@
 (defn make-child [p error-rate]
   (map #((:make-child %1) error-rate) p))
 
-(defn make-offspring [parent nr-children]
-  (let [children-copies (for [_ (range nr-children)]
-                          (make-child parent))]))
+(defn make-offspring [parent nr-children error-rate]
+  (for [_ (range nr-children)]
+    (make-child parent error-rate)))
 
 (defn draw-drawing [drawing]
   (q/no-stroke)
@@ -133,26 +124,55 @@
       (draw-shape c)
       (recur (first nxt) (rest nxt)))))
 
+(defn compare-images [target-img]
+  (let [dest-pixels (q/pixels target-img)
+        nr-pixels (count dest-pixels)
+        child-pixels (q/pixels)
+        child-nr-pixels (count child-pixels)]
+    (when (not= nr-pixels child-nr-pixels)
+      (throw (Exception. "The images do not have the same number of pixels")))
+    (as-> (map #(Math/abs (- %1 %2)) dest-pixels child-pixels) $
+          (reduce + $)
+          (Math/sqrt $))))
+
 (defn update [state]
-  (assoc state :population (->> state :population (drop 1))))
+  (println (str "Left in current population: " (count (:population state)) "\n"
+                "Best score so far: " (get @(:best state) 0) "\n"))
+  (let [next-drawing (first (:population state))]
+    (if (nil? next-drawing)
+      (let [next-population (make-offspring (get @(:best state) 1)
+                                            (:nr-children state)
+                                            0.95)]
+        (println (str "hierso: " (count next-population)))
+        (assoc state :population next-population))
+      (assoc state :population (drop 1 (:population state))))))
 
 (defn draw [state]
-  (let [drawing (-> state :population first)]
-    (println (str "hierso: " (count (:population state))))
-    (draw-drawing drawing)))
+  (let [drawing (-> state :population first)
+        best-placeholder (:best state)]
+    (draw-drawing drawing)
+    (let [score (compare-images (:img state))]
+      (if (nil? @best-placeholder)
+        (reset! best-placeholder [score drawing])
+        (swap! best-placeholder (fn [[old-score old-drawing]]
+                                  (if (< score old-score)
+                                    [score drawing]
+                                    [old-score old-drawing])))))))
 
 (defn make-drawing [path population-size nr-shapes]
   (let [setup-fn (fn []
-                   (q/frame-rate 5)
                    {:img (q/load-image path)
-                          :population (take 10 (repeatedly #(random-drawing nr-shapes)))})
+                    :nr-children population-size
+                    :best (atom nil)
+                    :population (take population-size
+                                      (repeatedly #(random-drawing nr-shapes)))})
         draw-fn draw
         sketch (q/sketch
                 :title "Image"
                 :setup setup-fn
                 :update update
                 :draw draw-fn
-                :size [901 588]
+                :size [200 200]
                 :middleware [m/fun-mode])]))
 
 (defn -main [& args])
