7622f692df4fdf2907e760cfa8d9916ef2059328
Pieter Breed <pieterbreed@Pieters-MacBook-Pro.local>
2016-06-02 15:34:48 +0200

    test checkin from within idea

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                   :creds :gpg}}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.datomic/datomic-pro "0.9.5372"]
                  [net.mikera/imagez "0.10.0"]
                  [org.clojure/core.async "0.2.374"]])
